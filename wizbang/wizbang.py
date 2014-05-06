ae9f567adac8713769e672100c05a0271d6687b6
Tom Henderson <tomhenderson@mac.com>
2014-05-06 16:52:42 +1200

    Outline for classes and methods

---
+++ wizbang/wizbang.py
@@ -0,0 +1,63 @@
+#!/bin/python
+
+import requests
+
+# Properties:
+# 	menu
+#		returns the entire menu
+# 	account_types
+#		returns a list of all account types and settings
+#
+# Methods:
+# 	order(customer, order, tender)
+# 	get_account(id=id, number=number)
+#		returns an Account object
+# 	get_invoice(id=id, number=number, outletid=outletid, txtcode=txtcode)
+#		returns invoice details
+# 	get_print_messages(outlet=outletid)
+#		returns a PrintMessages object
+
+class WizBang(object):
+	def __init__(self, server_url, server_port):
+		self.server_url = server_url
+		self.server_port = server_port
+
+	@property
+	def menu(self):
+		return
+
+	@property
+	def account_types(self):
+		return
+
+	def order(self, customer=None, order=None, tender=None):
+		return
+
+	def get_account(self, id=None, number=None):
+		return
+
+	def get_invoice(self, id=None, number=None, outlet=None, txtcode=None):
+		return
+
+	def get_print_messages(self, outlet=None):
+		return
+
+class Account(object):
+	def __init__(self):
+		return
+
+class PrintMessages(object):
+	def __init__(self):
+		return
+
+class Customer(object):
+	def __init__(self):
+		return
+
+class Order(object):
+	def __init__(self):
+		return
+
+class Tender(object):
+	def __init__(self):
+		return
